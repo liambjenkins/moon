a27bdfc75138e86438a2943a71ee689887bd0ff5
liambjenkins <liambjenkins@gmail.com>
2026-07-31 17:39:30 +1000

    Update transits.py

--- moon/transits.py
+++ moon/transits.py
@@ -1,2 +1,35 @@
-def get_transit(*args, **kwargs):
-    return None
+from datetime import datetime, timezone
+
+
+def get_zodiac_sign(longitude):
+
+    signs = [
+        "Aries",
+        "Taurus",
+        "Gemini",
+        "Cancer",
+        "Leo",
+        "Virgo",
+        "Libra",
+        "Scorpio",
+        "Sagittarius",
+        "Capricorn",
+        "Aquarius",
+        "Pisces",
+    ]
+
+    return signs[int(longitude // 30)]
+
+
+def get_transit(previous_longitude, current_longitude):
+
+    previous_sign = get_zodiac_sign(previous_longitude)
+    current_sign = get_zodiac_sign(current_longitude)
+
+    if previous_sign == current_sign:
+        return None
+
+    return {
+        "from": previous_sign,
+        "to": current_sign,
+    }
