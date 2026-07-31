9cb49690d02591d50129e287c4ab70a6d594777d
liambjenkins <liambjenkins@gmail.com>
2026-07-31 17:13:15 +1000

    Update feed.py

--- moon/feed.py
+++ moon/feed.py
@@ -1,3 +1,32 @@
-"""
-Moon module.
-"""
+from icalendar import Calendar, Event
+
+
+def build_feed(events):
+
+    calendar = Calendar()
+
+    calendar.add(
+        "prodid",
+        "-//Moon Calendar//"
+    )
+
+    calendar.add(
+        "version",
+        "2.0"
+    )
+
+    calendar.add(
+        "X-WR-CALNAME",
+        "Moon"
+    )
+
+    for event in events:
+        calendar.add_component(event)
+
+    return calendar
+
+
+def save_feed(calendar, filename="moon.ics"):
+
+    with open(filename, "wb") as file:
+        file.write(calendar.to_ical())
