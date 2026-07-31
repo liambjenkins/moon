8d6eff053d001107cf00fc9cdd6a1691fd7a7db9
liambjenkins <liambjenkins@gmail.com>
2026-07-31 18:23:20 +1000

    Update phases.py

--- moon/phases.py
+++ moon/phases.py
@@ -1,74 +1,45 @@
-import math
-
-
-MAJOR_PHASES = {
-    0: "New Moon",
-    90: "First Quarter",
-    180: "Full Moon",
-    270: "Last Quarter",
-}
-
-
-def normalise(angle):
-    return angle % 360
-
-
-def distance(a, b):
-
-    difference = abs(a - b)
-
-    return min(
-        difference,
-        360 - difference
-    )
-
-
 def get_phase(angle):
 
-    angle = normalise(angle)
+    angle = angle % 360
 
     if angle < 22.5:
         return "New Moon"
 
-    if angle < 67.5:
+    elif angle < 67.5:
         return "Waxing Crescent"
 
-    if angle < 112.5:
+    elif angle < 112.5:
         return "First Quarter"
 
-    if angle < 157.5:
+    elif angle < 157.5:
         return "Waxing Gibbous"
 
-    if angle < 202.5:
+    elif angle < 202.5:
         return "Full Moon"
 
-    if angle < 247.5:
+    elif angle < 247.5:
         return "Waning Gibbous"
 
-    if angle < 292.5:
+    elif angle < 292.5:
         return "Last Quarter"
 
-    if angle < 337.5:
-        return "Balsamic Moon"
+    elif angle < 337.5:
+        return "Waning Crescent"
 
-    return "New Moon"
+    else:
+        return "New Moon"
 
 
 def get_illumination(angle):
 
-    illuminated = (
-        1 - math.cos(math.radians(angle))
+    import math
+
+    illumination = (
+        1 - math.cos(
+            math.radians(angle)
+        )
     ) / 2
 
-    return round(illuminated * 100)
-
-
-def get_major_phase(angle):
-
-    for phase_angle, name in MAJOR_PHASES.items():
-
-        if distance(angle, phase_angle) < 1:
-
-            return name
-
-    return None
+    return round(
+        illumination * 100
+    )
