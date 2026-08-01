c7a42c0c16185100b61e8552add63595e27ac245
liambjenkins <liambjenkins@gmail.com>
2026-08-01 22:03:56 +1000

    Update celestial_events.py

--- moon/celestial_events.py
+++ moon/celestial_events.py
@@ -1,10 +1,21 @@
-from datetime import datetime, timezone
 from skyfield import almanac
 
 from moon.astronomy import ts, eph
 
 
-def find_seasonal_events(start_date, end_date):
+SEASON_NAMES = {
+    0: "March Equinox",
+    1: "June Solstice",
+    2: "September Equinox",
+    3: "December Solstice",
+}
+
+
+
+def find_seasonal_events(
+    start_date,
+    end_date,
+):
 
     events = []
 
@@ -12,41 +23,34 @@ def find_seasonal_events(start_date, end_date):
     start = ts.utc(
         start_date.year,
         start_date.month,
-        start_date.day
+        start_date.day,
     )
 
 
     end = ts.utc(
         end_date.year,
         end_date.month,
-        end_date.day
+        end_date.day,
     )
 
 
     times, seasons = almanac.find_discrete(
         start,
         end,
-        almanac.seasons(eph)
+        almanac.seasons(eph),
     )
-
-
-    names = {
-        0: "March Equinox",
-        1: "June Solstice",
-        2: "September Equinox",
-        3: "December Solstice",
-    }
 
 
     for time, season in zip(
         times,
-        seasons
+        seasons,
     ):
 
         events.append(
             {
-                "type": names[season],
-                "time": time.utc_datetime()
+                "type": SEASON_NAMES[season],
+
+                "time": time.utc_datetime(),
             }
         )
 
