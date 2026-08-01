6c10f7dc7f044c30f16c1a0f7fbdb0ad32381921
liambjenkins <liambjenkins@gmail.com>
2026-08-01 18:23:31 +1000

    Update phases.py

--- moon/phases.py
+++ moon/phases.py
@@ -1,52 +1,6 @@
 from math import cos, radians
 
 
-def get_phase(angle):
-
-    angle = angle % 360
-
-
-    # Exact lunar turning points
-    # These get their own single-day labels
-
-    if angle < 6 or angle >= 354:
-        return "New Moon"
-
-
-    if 84 <= angle <= 96:
-        return "First Quarter"
-
-
-    if 174 <= angle <= 186:
-        return "Full Moon"
-
-
-    if 264 <= angle <= 276:
-        return "Last Quarter"
-
-
-    # Transitional phases
-
-    if angle < 90:
-        return "Waxing Crescent"
-
-
-    if angle < 180:
-        return "Waxing Gibbous"
-
-
-    if angle < 270:
-        return "Waning Gibbous"
-
-
-    if angle < 315:
-        return "Waning Crescent"
-
-
-    return "Balsamic Moon"
-
-
-
 def get_illumination(angle):
 
     value = (
