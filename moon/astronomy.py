fefeb4f2c1d2020e71e4685379204fd61eb2e265
liambjenkins <liambjenkins@gmail.com>
2026-08-01 22:47:26 +1000

    Update astronomy.py

--- moon/astronomy.py
+++ moon/astronomy.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 from math import cos, radians
+
 from skyfield.api import load
 from skyfield.framelib import ecliptic_frame
 
@@ -23,7 +24,79 @@ def datetime_to_time(dt):
             tzinfo=timezone.utc
         )
 
-    return ts.from_datetime(dt)
+    return ts.from_datetime(
+        dt.astimezone(
+            timezone.utc
+        )
+    )
+
+
+
+def get_moon_position(t):
+
+    position = (
+        earth
+        .at(t)
+        .observe(moon)
+        .apparent()
+    )
+
+    latitude, longitude, _ = position.frame_latlon(
+        ecliptic_frame
+    )
+
+    return {
+        "longitude": longitude.degrees % 360,
+        "latitude": latitude.degrees,
+    }
+
+
+
+def get_sun_position(t):
+
+    position = (
+        earth
+        .at(t)
+        .observe(sun)
+        .apparent()
+    )
+
+    latitude, longitude, _ = position.frame_latlon(
+        ecliptic_frame
+    )
+
+    return {
+        "longitude": longitude.degrees % 360,
+        "latitude": latitude.degrees,
+    }
+
+
+
+def get_moon_longitude(t):
+
+    return get_moon_position(t)["longitude"]
+
+
+
+def get_moon_latitude(t):
+
+    return get_moon_position(t)["latitude"]
+
+
+
+def get_sun_longitude(t):
+
+    return get_sun_position(t)["longitude"]
+
+
+
+def get_phase_angle_at(t):
+
+    return (
+        get_moon_longitude(t)
+        -
+        get_sun_longitude(t)
+    ) % 360
 
 
 
@@ -40,76 +113,42 @@ def prepare_datetime(dt):
 
 
 
-def get_moon_longitude(t):
-
-    position = (
-        earth
-        .at(t)
-        .observe(moon)
-        .apparent()
-    )
-
-    _, longitude, _ = position.frame_latlon(
-        ecliptic_frame
-    )
-
-    return longitude.degrees % 360
-
-
-
-def get_sun_longitude(t):
-
-    position = (
-        earth
-        .at(t)
-        .observe(sun)
-        .apparent()
-    )
-
-    _, longitude, _ = position.frame_latlon(
-        ecliptic_frame
-    )
-
-    return longitude.degrees % 360
-
-
-
-def get_phase_angle_at(t):
-
-    return (
-        get_moon_longitude(t)
-        -
-        get_sun_longitude(t)
-    ) % 360
-
-
-
 def get_phase_angle(dt):
 
-    dt = prepare_datetime(dt)
-
     return get_phase_angle_at(
-        datetime_to_time(dt)
+        datetime_to_time(
+            prepare_datetime(dt)
+        )
     )
 
 
 
 def get_moon_longitude_at(dt):
 
-    dt = prepare_datetime(dt)
-
     return get_moon_longitude(
-        datetime_to_time(dt)
+        datetime_to_time(
+            prepare_datetime(dt)
+        )
+    )
+
+
+
+def get_moon_latitude_at(dt):
+
+    return get_moon_latitude(
+        datetime_to_time(
+            prepare_datetime(dt)
+        )
     )
 
 
 
 def get_sun_longitude_at(dt):
 
-    dt = prepare_datetime(dt)
-
     return get_sun_longitude(
-        datetime_to_time(dt)
+        datetime_to_time(
+            prepare_datetime(dt)
+        )
     )
 
 
@@ -127,20 +166,27 @@ def get_raw_moon_data(day):
 
 
     t = datetime_to_time(
-        local_time.astimezone(
-            timezone.utc
-        )
+        local_time
     )
 
 
-    angle = get_phase_angle_at(t)
+    angle = get_phase_angle_at(
+        t
+    )
+
+
+    moon_position = get_moon_position(
+        t
+    )
 
 
     return {
 
         "angle": angle,
 
-        "longitude": get_moon_longitude(t),
+        "longitude": moon_position["longitude"],
+
+        "latitude": moon_position["latitude"],
 
         "illumination": round(
             (
