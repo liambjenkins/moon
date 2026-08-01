b01d64c5452f58b52111202815a4f6f195a9bc6a
liambjenkins <liambjenkins@gmail.com>
2026-08-01 22:56:21 +1000

    Update formatter.py

--- moon/formatter.py
+++ moon/formatter.py
@@ -13,43 +13,55 @@ def format_time(value):
 
 
 
-def format_title(day: MoonDay):
+def format_title(
+    day: MoonDay
+):
 
     return (
         f"{day.phase} "
-        f"in "
-        f"{day.sign}"
+        f"in {day.sign}"
     )
 
 
 
-def format_notes(day: MoonDay):
+def format_notes(
+    day: MoonDay
+):
 
     lines = []
 
 
-    phase_line = (
+    #
+    # Moon event detail
+    #
+
+    title = (
         f"{day.phase} "
-        f"in "
-        f"{day.sign}"
+        f"in {day.sign}"
     )
 
 
     if day.phase_time:
 
-        phase_line += (
+        title += (
             f", "
             f"{format_time(day.phase_time)}"
         )
 
 
     lines.append(
-        phase_line
+        title
     )
 
 
-    lines.append("")
+    lines.append(
+        ""
+    )
 
+
+    #
+    # Lunar information
+    #
 
     lines.append(
         f"Lunar Day {day.lunar_day}, "
@@ -57,24 +69,50 @@ def format_notes(day: MoonDay):
     )
 
 
+    #
+    # Moon sign transition
+    #
+
     if day.sign_transition:
 
-        lines.append("")
+        lines.append(
+            ""
+        )
 
 
         lines.append(
-            f"{day.sign_transition['from']}"
-            f" → "
-            f"{day.sign_transition['to']}"
-            f", "
+            f"{day.sign_transition['from']} "
+            f"→ "
+            f"{day.sign_transition['to']}, "
             f"{format_time(day.sign_transition['time'])}"
         )
 
 
-    # Coming will be added here
+    #
+    # Coming events
+    #
+
+    if day.coming:
+
+        lines.append(
+            ""
+        )
+
+        lines.append(
+            "Coming:"
+        )
 
 
-    lines.append("")
+        for event in day.coming:
+
+            lines.append(
+                event
+            )
+
+
+    lines.append(
+        ""
+    )
 
 
     lines.append(
